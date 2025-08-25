9becefa6ee751d2e0e7ea2ca81e48891c2371526
pcboy <ravimohan1991@gmail.com>
2025-08-25 19:30:56 +0530

    Directory restructuring

--- Emulator/main.cpp
+++ Emulator/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     Karma::Log::Init();
     
-	auto emulator = std::make_unique<Bus>("./bios/SCPH1001.BIN");
+    auto emulator = std::make_unique<Bus>("/home/the-cowboy/PSXemu/bios/SCPH1001.BIN");
 
     std::string game_file = "./roms/RIDGERACERUSA.CUE";
 	emulator->cddrive->insert_disk(game_file);
